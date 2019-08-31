3a7e346dc950f2364ce96fd4d91847d24a710ae9
hyntk <hyntkjp@gmail.com>
2019-08-31 15:37:56 +0900

    adw検証のため

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'tweets/index'
+  # get 'tweets/index'
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' } # ここを変更
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
