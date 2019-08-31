924b11e97a2770ac8db5a61a44a7e79303b9bdd7
hyntk <hyntkjp@gmail.com>
2019-08-31 16:32:46 +0900

    adw検証ファイルの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' } # ここを変更
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # root 'tweets#index'
+  root 'tweets#index'
 
   resources :maps
   resources :blogs
